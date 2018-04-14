bf7b25e897b80ae902f1f701c053ab8c4d99f6a0
xushubo <tmac523@163.com>
2018-04-14 17:49:47 +0800

    updated 2018-4-14

--- pages/index/index.js
+++ pages/index/index.js
@@ -20,6 +20,24 @@ var snakeHead = {
 
 }
 
+//身体对象（数组）
+var snakeBodys = [];
+
+//食物的对象(数组)
+var foods = [];
+
+//窗口宽高
+var windowWidth = 0;
+var windowHeight = 0;
+
+//用于确定是否删除
+var collideBol = true;
+
+//手指移动的方向
+var direction = null;
+
+//蛇移动的方向
+var snakeDirection = 'right';
 
 Page({
   canvasStart: function (e) {
@@ -34,34 +52,145 @@ Page({
     Y = moveY - startY;
 
     if (Math.abs(X) > Math.abs(Y) && X > 0) {
-      console.log('right');
+      direction = 'right';
     }else if (Math.abs(X) > Math.abs(Y) && X < 0) {
-      console.log('left');
+      direction = 'left';
     } else if (Math.abs(Y) > Math.abs(X) && Y > 0) {
-      console.log('bottom');
+      direction = 'bottom';
     } else if (Math.abs(Y) > Math.abs(X) && Y < 0) {
-      console.log('top');
+      direction = 'top';
     }
+  },
+  canvasEnd: function () {
+    snakeDirection = direction;
   },
   onReady: function () {
     //获取画布的上下文
     var context = wx.createContext('snakeCanvas');
+    //帧数
+    var frameNum = 0;
 
-    function animate () {
-
-      context.setFillStyle(snakeHead.color);
+    function draw (obj) {
+      context.setFillStyle(obj.color);
       context.beginPath();
-      context.rect(snakeHead.x, snakeHead.y, snakeHead.w, snakeHead.h);
+      context.rect(obj.x, obj.y, obj.w, obj.h);
       context.closePath();
       context.fill();
+    }
+    //碰撞函数
+    function collide (obj1, obj2) {
+      var l1 = obj1.x;
+      var r1 = l1 + obj1.w;
+      var t1 = obj1.y;
+      var b1 = t1 + obj1.h;
+      
+      var l2 = obj2.x;
+      var r2 = l2 + obj2.w;
+      var t2 = obj2.y;
+      var b2 = t2 + obj2.h;
+      if (l2 < r1 && r2 > l1 && t2< b1 && b2 > t1) {
+        return true;
+      }else {
+        return false;
+      }
+
+    }
+
+
+    function animate () {
+      frameNum ++;
+      if (frameNum % 10 == 0) {
+        //向蛇身体数组添加一个蛇头的上一个的位置（身体对象）
+        snakeBodys.push({
+          x: snakeHead.x,
+          y: snakeHead.y,
+          w: 20,
+          h: 20,
+          color: '#00ff00'
+        });
+
+        if (snakeBodys.length > 6) {
+          if (collideBol) {
+            //移除不用的身体位置
+            snakeBodys.shift();
+          }else {
+            collideBol = true;
+          }
+        }
+        switch (snakeDirection) {
+          case 'left':
+            snakeHead.x -= snakeHead.w;
+            break;
+          case 'right':
+            snakeHead.x += snakeHead.w;
+            break;
+          case 'bottom':
+            snakeHead.y += snakeHead.h;
+            break;
+          case 'top':
+            snakeHead.y -= snakeHead.h;
+            break;
+        }
+      }
+      
+      //绘制蛇头
+      draw(snakeHead);
+
+      //绘制蛇身
+      for (var i=0; i<snakeBodys.length; i++) {
+        var snakeBody = snakeBodys[i]
+        draw(snakeBody);
+      }
+      //绘制食物
+      for (var i=0; i<foods.length; i++) {
+        var foodObj = foods[i];
+        draw(foodObj);
+        if (collide(snakeHead, foodObj)) {
+          console.log('撞上');
+          collideBol = false;
+          foodObj.reset();
+        }
+      }
 
       wx.drawCanvas({
         canvasId: 'snakeCanvas',
         actions: context.getActions()
       });
       
-      requestAnimationFrame(animate);
+      setTimeout(animate, 30);
     }
-    animate();
+    function rand (min, max) {
+      return parseInt(Math.random()*(max-min)) + min;
+    }
+
+    //构造食物对象
+    function Food () {
+      this.x = rand(0, windowWidth);
+      this.y = rand(0, windowHeight);
+      var w = rand(10, 20);
+      this.w = w;
+      this.h = w;
+      this.color = 'rgb('+rand(0, 255)+', '+rand(0, 255)+', '+rand(0, 255)+')';
+      this.reset = function () {
+        this.x = rand(0, windowWidth);
+        this.y = rand(0, windowHeight);
+        this.color = 'rgb(' + rand(0, 255) + ', ' + rand(0, 255) + ', ' + rand(0, 255) + ')';
+      }
+    }
+
+    wx.getSystemInfo({
+      success: function (res) {
+        windowWidth = res.windowWidth;
+        windowHeight = res.windowHeight;
+
+        for (var i=0; i<20; i++) {
+          var foodObj = new Food();
+          foods.push(foodObj);
+        }
+
+        animate();
+      },
+    })
+    
   }
 })
